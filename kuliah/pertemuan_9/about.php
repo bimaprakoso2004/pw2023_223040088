dfaff8491463f8a4b991e783cab90fd568baeda1
bimaprakoso2004 <bimaprakoso@gmail.com>
2023-05-11 11:14:16 +0700

    tubes

---
+++ kuliah/pertemuan_9/about.php
@@ -0,0 +1,18 @@
+
+<?php
+$students = [
+    [
+        "nama" => "Bima Hafit Prakoso",
+        "npm" => "223040088",
+        "email" => "bimaprakoso50@gmail.com"
+    ],
+
+    [
+        "nama" => "Meutuah Dicco Linge",
+        "npm" => "223040098",
+        "email" => "diccolinge@gmail.com"
+    ]
+    ];
+
+    require('views/about.view.php')
+?>
---
+++ kuliah/pertemuan_9/views/index.view.php
@@ -0,0 +1,49 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+  </head>
+  <body>
+    
+  <nav class="navbar navbar-expand-lg bg-light">
+  <div class="container">
+    <a class="navbar-brand" href="#">Beemz</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="about.php">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contact</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled">Disabled</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<div class="container">
+    <h1 class="mt-2">Halaman Home</h1>
+    
+    <h3>Daftar Mahasiswa</h3>
+    <?php foreach($students as $student) : ?>
+        <ul>
+            <li>nama : <?= $student["nama"]; ?></li>
+            <li>npm : <?= $student["npm"]; ?></li>
+            <li>email : <?= $student["email"]; ?></li>
+        </ul>
+        <?php endforeach; ?>
+</div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+  </body>
+</html>
